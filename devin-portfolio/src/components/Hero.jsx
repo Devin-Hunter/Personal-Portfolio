8bcbfc57927f5e57a7d267d58ef433355b61912c
Devin-Matherne <devinmatherne@ymail.com>
2024-09-04 19:34:39 -0600

    Changed CSS settings in Page.tsx file

--- devin-portfolio/src/components/Hero.jsx
+++ devin-portfolio/src/components/Hero.jsx
@@ -6,9 +6,10 @@ const Hero = () => {
     <div className="pb-20">
       <div>
         <Vortex className="top-38 left-80 h-[80vh] w-[50vw]" fill="bg-black"/>
+        <div  className="h-screen w-full bg-black relative flex items-center justify-center px-0"/>
       </div>
 
-      <div  className="h-screen w-full bg-black relative flex items-center justify-center px-0"/>
+
     </div>
   )
 }
