834fc2afae594b2321b9e4c348ac0532bef0f0f2
Devin-Matherne <devinmatherne@ymail.com>
2024-09-07 18:50:41 -0600

    Added white dot background to hero

--- devin-portfolio/src/components/Hero.jsx
+++ devin-portfolio/src/components/Hero.jsx
@@ -5,11 +5,16 @@ const Hero = () => {
   return (
     <div className="pb-20">
       <div>
-        <Vortex className="top-38 left-80 h-[80vh] w-[50vw]" fill="bg-black"/>
-        <div  className="h-screen w-full bg-black relative flex items-center justify-center px-0"/>
-          <p className="text-center text-white">Devin Hunter</p>
+        <Vortex className="top-38 left-80 h-[80vh] w-[50vw]"/>
+
       </div>
 
+      <div>
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.3] bg-dot-black/[0.2] relative flex items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+      </div>
 
     </div>
   )
