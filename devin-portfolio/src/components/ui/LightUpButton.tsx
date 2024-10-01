84527c595cd4caa5559999c4e0eb09486baceb03
Devin-Matherne <devinmatherne@ymail.com>
2024-10-01 13:00:59 -0600

    Changed Hero button color

--- devin-portfolio/src/components/ui/LightUpButton.tsx
+++ devin-portfolio/src/components/ui/LightUpButton.tsx
@@ -7,7 +7,7 @@ export const LightUpButton = ({
 }) => {
   return (
     <button className="p-[3px] mt-4 relative">
-        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+        <div className="absolute inset-0 bg-gradient-to-r from-green-700 to-teal-500 rounded-lg" />
         <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
             {title}
         </div>
